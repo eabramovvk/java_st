4b51eed70736e9928b79ea9d55f6fb374f16014c
eabramovvk <87018662+eabramovvk@users.noreply.github.com>
2021-07-17 18:02:51 +0300

    Second task
    Сделано задание №2

---
+++ sandbox/src/main/java/ru/stqa/st/sandbox/SecondTask.java
@@ -0,0 +1,10 @@
+package ru.stqa.st.sandbox;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        Point p1 = new Point(4,9);
+        Point p2 = new Point(7,3);
+
+        System.out.println("Расстояние между двумя точками = " + p1.distance(p1,p2));
+    }
+}
