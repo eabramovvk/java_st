ab577825141afc68e0cbabe06e3bbf42b6129cfa
eabramovvk <87018662+eabramovvk@users.noreply.github.com>
2021-08-22 21:07:31 +0300

    before task 9

--- addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/tests/GroupCreationTests.java
@@ -1,7 +1,12 @@
 package ru.stqa.st.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.st.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 
 public class GroupCreationTests extends TestBase {
@@ -9,10 +14,16 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-  }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
+    before.add(group);
+    Comparator<? super GroupData> byId =(g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
+  }
 }
