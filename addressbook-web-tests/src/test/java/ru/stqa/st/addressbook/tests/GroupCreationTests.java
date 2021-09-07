2fa4195ab8cc51d8c863ac9e283a8d215c731ba4
eabramovvk <87018662+eabramovvk@users.noreply.github.com>
2021-09-07 16:27:28 +0300

    log stuff

--- addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.st.addressbook.tests;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 import ru.stqa.st.addressbook.model.GroupData;
 import ru.stqa.st.addressbook.model.Groups;
@@ -16,49 +18,49 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
-
 public class GroupCreationTests extends TestBase {
     private Properties properties;
 
 
     @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
-    try (BufferedReader reader = new BufferedReader( new FileReader(new File("src/test/resources/groups.xml")))){
-        String xml = "";
-        String line = reader.readLine();
-        while (line != null){
-            xml += line;
-            line = reader.readLine();
+    public Iterator<Object[]> validGroups() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
+            String xml = "";
+            String line = reader.readLine();
+            while (line != null) {
+                xml += line;
+                line = reader.readLine();
+            }
+            XStream xStream = new XStream();
+            xStream.processAnnotations(GroupData.class);
+            List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
+            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
-        XStream xStream = new XStream();
-        xStream.processAnnotations(GroupData.class);
-        List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
-        return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
+
     }
 
-  }
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
-  @Test(dataProvider = "validGroups")
-  public void testGroupCreation(GroupData group) throws Exception {
-      app.goTo().groupPage();
-      Groups before = app.group().all();
-      app.group().create(group);
-      assertThat(app.group().count(), equalTo(before.size()+1));
-      Groups after = app.group().all();
-      assertThat(after, equalTo(
-              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-  @Test
-  public void testBadGroupCreation() throws IOException {
-      properties = new Properties();
-      properties.load(new FileReader(new File(String.format("src/test/resources/local.properties"))));
-      app.goTo().groupPage();
-      Groups before = app.group().all();
-      GroupData group = new GroupData().withName(properties.getProperty("web.badGroupName"));
-      app.group().create(group);
-      assertThat(app.group().count(), equalTo(before.size()));
-      Groups after = app.group().all();
-      assertThat(after, equalTo(before));
-  }
+    @Test
+    public void testBadGroupCreation() throws IOException {
+        properties = new Properties();
+        properties.load(new FileReader(new File(String.format("src/test/resources/local.properties"))));
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName(properties.getProperty("web.badGroupName"));
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+    }
 }
