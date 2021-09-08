b457d58a3497f91439551e9253919803895a6c11
eabramovvk <87018662+eabramovvk@users.noreply.github.com>
2021-09-08 18:26:52 +0300

    before task 17
    new module added for mantis tests

--- addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/generators/ContactDataGenerator.java
@@ -61,7 +61,7 @@ public class ContactDataGenerator {
         System.out.println(new File(".").getAbsolutePath());
         try (Writer writer  = new FileWriter(file)){
             for (ContactData contact : contacts) {
-                writer.write(String.format("%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress(), contact.getEmail(),contact.getGroups()));
+                writer.write(String.format("%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress(), contact.getEmail(),contact.getGroups()));
             }
         }
 
