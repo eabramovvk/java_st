f268b9a2df83a145a878d76b43b328037336dc2c
eabramovvk <87018662+eabramovvk@users.noreply.github.com>
2021-09-14 02:51:40 +0300

    10.3

--- addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/st/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.stqa.st.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -45,6 +46,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "mac")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
 
             wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
